454867bd2a0950a35ba87d46f0fb4531f3e3f9df
Radovan Tvrdak <radovan.tvrdak@gmai.com>
2022-02-27 16:08:31 +0100

    Modified original library to work with python3 Added starting function Modified README.md

---
+++ main.py
@@ -0,0 +1,41 @@
+from sipfullproxy import UDPHandler
+from socketserver import UDPServer
+import socket
+from threading import Thread
+import time
+import logging
+
+
+def main():
+    port = 5060
+
+    logging.basicConfig(filename="sip.log", encoding="utf-8", level=logging.INFO,
+                        format='%(asctime)s:%(levelname)s:%(message)s', datefmt='%H:%M:%S')
+    logging.info("STARTED - " + time.strftime("%a, %d %b %Y %H:%M:%S ", time.localtime()))
+    hostname = socket.gethostname()
+    ip = socket.gethostbyname(hostname)
+    logging.info(hostname)
+    logging.info(f"{ip}:{port}")
+
+    with UDPServer((ip, port), UDPHandler) as proxy:
+        proxy.recordroute = f"Record-Route: <sip:{ip}:{port};lr>"
+        proxy.topvia = f"Via: SIP/2.0/UDP {ip}:{port}"
+        proxy.logging = logging
+
+        server_thread = Thread(None, proxy.serve_forever)
+        server_thread.daemon = True
+        server_thread.start()
+
+        while input("Type '.q' to exit: ") != ".q":
+            continue
+
+        proxy.shutdown()
+        proxy.server_close()
+        server_thread.join()
+
+    logging.info("ENDED - " + time.strftime("%a, %d %b %Y %H:%M:%S ", time.localtime()))
+    exit(0)
+
+
+if __name__ == "__main__":
+    main()
